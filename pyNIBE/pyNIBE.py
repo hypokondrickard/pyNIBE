9638d6651ce0dddddb8946972a84514885901f9a
Rickard Östman <rost@spotify.com>
2015-12-27 12:10:18 +0100

    added result parsing, made system_id user configurable

--- pyNIBE/pyNIBE.py
+++ pyNIBE/pyNIBE.py
@@ -1,13 +1,16 @@
+from BeautifulSoup import BeautifulSoup
 import requests
+import re
 
 class pyNIBE(object):
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, system_id):
         """
 
         """
         self.username = username
         self.password = password
+        self.system_id = system_id
         self.session = None
 
     def _login(self):
@@ -31,8 +34,45 @@ class pyNIBE(object):
         self.session.post('https://www.nibeuplink.com/LogIn', headers=headers, data=data)
 
     def _get(self):
-        r = requests.get('https://www.nibeuplink.com/System/21264/Status/ServiceInfo', cookies=self.session.cookies.get_dict())
-        self.readings = r.content
+        r = requests.get('https://www.nibeuplink.com/System/%s/Status/ServiceInfo' % self.system_id, cookies=self.session.cookies.get_dict())
+
+        soup = BeautifulSoup(r.content)
+
+        all_table_names = soup.findAll('h3')
+        all_tables = soup.findAll('table', {"class": 'Sortable {sortlist: [[0,0]]}'})
+
+        result = dict()
+
+        table_id = 0
+
+        for table in all_tables:
+            # get keys
+            var_designations = [x.text for x in table.findAll('span', attrs={'class': 'VariableDesignation'})]
+
+            # get values
+            var_values = [x.text for x in table.findAll(attrs={'class': re.compile(r".*(AutoUpdateValue|ID0).*")})]
+
+            # get descriptors
+            var_descriptors = []
+            table_body = table.find('tbody')
+            for row in table_body.findAll('tr'):
+                cols = [ele.contents[0].strip() for ele in row.findAll('td', limit=1)]
+                var_descriptors.append(cols[0])
+
+            # collapse result lists for this table
+            this_table = zip(var_designations, var_values, var_descriptors)
+
+            # populate result dict for this table
+            table_name = all_table_names[table_id].text
+            table_result = dict()
+            for entry in this_table:
+                table_result[entry[0]] = {'value': entry[1], 'decorator': entry[2]}
+
+            result[table_name] = table_result
+
+            table_id += 1
+
+        self.readings = result
 
     def open(self):
         self._login()
